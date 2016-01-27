1a5c66b50da8538105400f52a0cf6ac9717c6d59
Rachel Lee <lee.rachel.jy@gmail.com>
2016-01-27 09:51:11 -0500

    implement interactive fizzbuzz

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,28 +1,8 @@
-var fizzBuzzOnConsole = function(){
-    
-    for(var i = 1; i <= 100; i += 1) {
-        
-        var str = "";
-        
-        if(i % 3 === 0) {
-            str += "Fizz";
-        }
-        
-        if (i % 5 === 0) {
-            str += "Buzz";
-        }
-        
-        console.log(str || i);
-        
-    }
-    
-};
-
-var fizzBuzz = function(){
+var fizzBuzz = function(num){
     
     var strToReturn = "";
 
-    for(var i = 1; i <= 100; i += 1) {
+    for(var i = 1; i <= num; i += 1) {
         
         var str = "";
         
@@ -42,6 +22,52 @@ var fizzBuzz = function(){
     
 };
 
-fizzBuzzOnConsole();
+var fizzBuzzOnConsole = function(num){
+    
+    for(var i = 1; i <= num; i += 1) {
+        
+        var str = "";
+        
+        if(i % 3 === 0) {
+            str += "Fizz";
+        }
+        
+        if (i % 5 === 0) {
+            str += "Buzz";
+        }
+        
+        console.log(str || i);
+        
+    }
+    
+};
 
-document.getElementById("demo").innerHTML = fizzBuzz();
+var upperLimit = prompt("Please type an integer between 1 and 100");
+
+
+if(upperLimit > 1 && upperLimit % 1 === 0 && upperLimit !== NaN) {
+
+    var number = parseInt(upperLimit);
+    
+    document.getElementById("demo").innerHTML = fizzBuzz(number);
+
+    fizzBuzzOnConsole(number);
+
+}
+
+else {
+
+    alert("Try again!");
+
+    upperLimit = prompt("Please type an integer between 1 and 100");
+
+    var number = parseInt(upperLimit);
+
+    document.getElementById("demo").innerHTML = fizzBuzz(parseInt(upperLimit));
+    
+    fizzBuzzOnConsole(number);
+
+}
+
+
+
